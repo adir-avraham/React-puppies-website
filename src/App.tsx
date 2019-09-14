0613f15bd50e426d9692ef79076be5e2a45012fc
adir-avraham <adir.avraham@gmail.com>
2019-09-14 17:50:08 +0300

    img-comp-1

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,9 @@ const App: React.FC = () => {
     <div className="App">
       <Header style={{color: "#61DAFB", border: "2px solid grey", padding: "40px"}} title="Welcome to my best puppies website ever!"/>
       <header className="App-header">
-      <Images/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <Images height="300" width="300"/>
+       
+
 
       </header>
           <Footer style={{color: "red"}} title="All Rights Reserved (c)"/>
--- src/components/images/index.tsx
+++ src/components/images/index.tsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import pic1 from '..//images/1.jpg';
+import pic2 from '..//images/2.jpg'; 
+import pic3 from '..//images/3.jpg'; 
+import pic4 from '..//images/4.jpg'; 
+import pic5 from '..//images/5.jpg'; 
+import logoJpg from '..//images/logo.jpg'; 
+import logoPng from '..//images/logo.png'; 
 
 
 export default class Images extends React.Component<any, any> {
@@ -10,24 +17,14 @@ export default class Images extends React.Component<any, any> {
     }
 
     render() {
-        return <img src={this.props.link}></img>
+        return <div>
+            <img height={this.props.height} width={this.props.width} src={pic1}></img>       
+  
+
+        </div>
     }
 }
 
 
 
 
-
-
-// export default class Images extends React.Component<any, any> {
-
-//     constructor(props: any) {
-//         super(props)
-        
-        
-//     }
-
-//     render() {
-//         return <img src={this.props} placeholder={this.props.place} value={this.props.input} />
-//     }
-// }
